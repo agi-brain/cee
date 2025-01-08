79aa0abd186e20078d446d9048db97182748b871
wenzhangliu <liu_wzh@foxmail.com>
2025-01-08 20:15:43 +0800

    set device for rnd

--- ppo_atari.py
+++ ppo_atari.py
@@ -77,7 +77,7 @@ def train(config, log_path):
     if use_rnd_curiosity:
         input_channels = 4  # Grayscale image, should be the same as n_stack.
         output_dim = 512  # Example output dimension
-        target_network, predictor_network, optimizer = initialize_rnd(input_channels, output_dim)
+        target_network, predictor_network, optimizer = initialize_rnd(input_channels, output_dim, config.device)
         rnd_callback = RNDCustomCallback(target_network, predictor_network, optimizer)
     else:
         rnd_callback = None
--- pureppo/rnd.py
+++ pureppo/rnd.py
@@ -1,16 +1,17 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from typing import Union
 from stable_baselines3.common.callbacks import BaseCallback
 
 
 class RNDNetwork(nn.Module):
-    def __init__(self, input_channels=1, output_dim=512, device=torch.device('auto')):
+    def __init__(self, input_channels=1, output_dim=512, device: Union[torch.device, str] = "auto"):
         super(RNDNetwork, self).__init__()
-        self.conv1 = nn.Conv2d(input_channels, 32, kernel_size=8, stride=4)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-        self.fc1 = nn.Linear(7 * 7 * 64, output_dim)
+        self.conv1 = nn.Conv2d(input_channels, 32, kernel_size=8, stride=4, device=device)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, device=device)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, device=device)
+        self.fc1 = nn.Linear(7 * 7 * 64, output_dim, device=device)
 
     def forward(self, x):
         x = torch.relu(self.conv1(x))
@@ -21,9 +22,9 @@ class RNDNetwork(nn.Module):
         return x
 
 
-def initialize_rnd(input_channels=1, output_dim=512):
-    target_network = RNDNetwork(input_channels, output_dim)
-    predictor_network = RNDNetwork(input_channels, output_dim)
+def initialize_rnd(input_channels=1, output_dim=512, device: Union[torch.device, str] = "auto"):
+    target_network = RNDNetwork(input_channels, output_dim, device)
+    predictor_network = RNDNetwork(input_channels, output_dim, device)
 
     # Freeze the target network parameters
     for param in target_network.parameters():
